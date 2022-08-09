af649b49a21abea7a36a43f377c7b8813b09b123
wadeagbo <waheed.adeagbo@gmail.com>
2022-08-09 14:57:38 +0200

    ongoing exercise

--- Exercises/ex1/exercise.py
+++ Exercises/ex1/exercise.py
@@ -1,4 +1,9 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
+# Start the exercise 
 #
+print()
+print("Hello", "world", sep=" - ")
+print(type("Hello"))
+print(isinstance(123,int))
