c0127828bcd00465fc6835c3ea40af7739bbc5e2
wadeagbo <waheed.adeagbo@gmail.com>
2022-08-17 16:01:19 +0200

    correct exer6

--- Ex_project/MATRIX/OTS/var.py
+++
@@ -1,7 +0,0 @@
-OPERATION      = 'AWESOME NAME'
-OVERWATCH      = '+4917624806086'
-CHECKPOINTS    = ['ALPHA', 'BRAVO', 'CHARLIE', 'DELTA', 'ECHO', 'FOXTROT', 'GOLF', 'HOTEL', 'INDIA', 'JULIET', 'KILO', 'LIMA', 'MIKE', 'NOVEMBER', 'OSCAR', 'PAPA', 'QUEBEC', 'ROMEO', 'SIERRA', 'TANGO', 'UNIFORM', 'VICTOR', 'WHISKEY', 'XRAY', 'YANKEE', 'ZULU']
-URL            = 'http://4a34-88-68-190-16.ngrok.io'
-timeT          = '0:03:00'
-timeC          = '0:03:00'
-mode           = 'TEST'
